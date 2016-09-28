74131aaabb8d6d752f28bfedabd41e012de396ba
Vladimir Bauer <vbauerster@gmail.com>
2016-09-28 23:10:40 +0330

    fzf preview with highlight

--- functions.zsh
+++ functions.zsh
@@ -10,7 +10,7 @@ fzf-gt() {
   is_in_git_repo || return
   git tag --sort -version:refname |
   fzf-tmux --multi --preview-window right:70% \
-    --preview 'git show --color=always {} | head -'$LINES
+    --preview 'git show --color=always {} | head -200'
 }
 
 fzf-gh() {
@@ -18,7 +18,7 @@ fzf-gh() {
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph |
   fzf-tmux --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -200' |
   grep -o "[a-f0-9]\{7,\}"
 }
 
@@ -27,7 +27,7 @@ fzf-gf() {
   git -c color.status=always status --short |
   fzf-tmux -m --ansi --nth 2..,.. \
     --preview 'NAME="$(cut -c4- <<< {})" &&
-               (git diff --color=always "$NAME" | sed 1,4d; cat "$NAME") | head -'$LINES |
+               (git diff --color=always "$NAME" | sed 1,4d; cat "$NAME") | head -200' |
   cut -c4-
 }
 
@@ -35,7 +35,7 @@ fzf-gb() {
   is_in_git_repo || return
   git branch -a --color=always | grep -v '/HEAD\s' | sort |
   fzf-tmux --ansi --multi --tac --preview-window right:70% \
-    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -200' |
   sed 's/^..//' | cut -d' ' -f1 |
   sed 's#^remotes/##'
 }
@@ -44,7 +44,7 @@ fzf-gr() {
   is_in_git_repo || return
   git remote -v | awk '{print $1 " " $2}' | uniq |
   fzf-tmux --tac \
-    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(cut -d" " -f1 <<< {}) | head -'$LINES |
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(cut -d" " -f1 <<< {}) | head -200' |
   cut -d' ' -f1
 }
 
@@ -95,7 +95,7 @@ zghh() {
   fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
       --header "Press CTRL-S to toggle sort" \
       --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
-                 xargs -I % sh -c 'git show --color=always % | head -$LINES '" \
+                 xargs -I % sh -c 'git show --color=always % | head -200 '" \
       --bind "enter:execute:echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
               xargs -I % sh -c 'nvim fugitive://\$(git rev-parse --show-toplevel)/.git//% < /dev/tty'"
 }
