4afd8ae11cefa7b820a018f08cfb37578cdcdfcd
Vladimir Bauer <vbauerster@gmail.com>
2016-10-12 12:43:53 +0330

    Handle deleted files

--- functions.zsh
+++ functions.zsh
@@ -26,8 +26,8 @@ fzf-gf() {
   is_in_git_repo || return
   git -c color.status=always status --short |
   fzf-tmux -m --ansi --nth 2..,.. \
-    --preview '(git diff --color=always {2} | sed 1,4d; cat {2}) | head -300' |
-  cut -c4-
+    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+  cut -c4- | sed 's/.* -> //'
 }
 
 fzf-gb() {
