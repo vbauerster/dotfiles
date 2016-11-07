82b9044d2ad0fcd2fa0ad79576af15a18573126a
Vladimir Bauer <vbauerster@gmail.com>
2016-11-07 10:19:46 +0330

    update install script

--- config/nvim/install.sh
+++ config/nvim/install.sh
@@ -1,6 +1,6 @@
 for rcfile in $PWD/*; do
   f=$(basename $rcfile)
-  if [[ -d "$rcfile" || "$f" == $(basename $0) ]]; then
+  if [[ "$f" == $(basename $0) ]]; then
     continue
   fi
   ln -s "$rcfile" "$HOME/.config/nvim/$f"
