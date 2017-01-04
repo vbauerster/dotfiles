e50d2a907bc34c54fb27495d182e7c1003b640e4
Vladimir Bauer <vbauerster@gmail.com>
2017-01-04 23:59:35 +0500

    rcrc post hook

--- hooks/post-up/mkplugged.sh
+++ hooks/post-up/mkplugged.sh
@@ -1,1 +1,6 @@
-[[ ! -e $HOME/.config/nvim/plugged ]] && mkdir -p $HOME/.config/nvim/plugged
+PLUGGED=~/.config/nvim/plugged
+PLUG=~/.config/nvim/autoload/plug.vim
+
+[[ ! -e $PLUGGED ]] && mkdir -p $PLUGGED
+
+[[ ! -e $PLUG ]] && curl -fLo $PLUG --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
