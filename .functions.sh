4289d7f91069714a3be8cde117e73b367df1e12e
Vladimir Bauer <vbauerster@gmail.com>
2017-01-05 22:18:28 +0500

    --color-always

--- .functions.sh
+++ .functions.sh
@@ -15,7 +15,7 @@ fzf-gt() {
 
 fzf-gh() {
   is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph |
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color-always |
   fzf-tmux --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -200' |
