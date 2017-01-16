41990a3121f803dd172378a8140fcf6e7fcca127
Vladimir Bauer <vbauerster@gmail.com>
2017-01-16 15:16:56 +0500

    map ctrl ;,. to cmd ;,.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -34,6 +34,10 @@ end
 -- Bind the Hyper key
 f19 = hs.hotkey.bind({}, 'F19', pressedF19, releasedF19)
 
+hs.hotkey.bind({"ctrl"}, ".", nil, function() hs.eventtap.keyStroke("cmd", ".") end, nil, nil)
+hs.hotkey.bind({"ctrl"}, ",", nil, function() hs.eventtap.keyStroke("cmd", ",") end, nil, nil)
+hs.hotkey.bind({"ctrl"}, ";", nil, function() hs.eventtap.keyStroke("cmd", ";") end, nil, nil)
+
 -- require 'caffeine'
 -- require 'clipboard'
 -- require 'launch-applications'
