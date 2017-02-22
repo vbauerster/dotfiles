d6649637a21126ea67af70425b80ca09de7c6329
Vladimir Bauer <vbauerster@gmail.com>
2017-02-22 15:24:33 +0500

    hyper key mappings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,40 +5,65 @@ hyper = { "cmd", "alt", "shift", "ctrl" }
 -- A global variable for the sub-key Hyper Mode
 k = hs.hotkey.modal.new({}, 'F18')
 
--- Hyper+key for all the below are setup somewhere
--- The handler already exists, usually in Keyboard Maestro
--- we just have to get the right keystroke sent
-hyperBindings = {'H','C','J','K','M','T','P','SPACE','+','-','LEFT','RIGHT'}
+-- Hyper+key bindings for external handlers
+extHyperBindings = {'a', 'o', 'u', 'e', 'j', 'k', 'p', ',', '.', 'space', '+', '-'}
 
-for i,key in ipairs(hyperBindings) do
+for i,key in ipairs(extHyperBindings) do
   k:bind({}, key, nil, function() hs.eventtap.keyStroke(hyper, key)
     k.triggered = true
   end)
 end
 
--- Enter Hyper Mode when F19 (left control) is pressed
-pressedF19 = function()
+k:bind({}, 'h', nil, function() hs.eventtap.keyStroke({}, 'left') k.triggered = true end)
+k:bind({}, 'n', nil, function() hs.eventtap.keyStroke({}, 'right') k.triggered = true end)
+k:bind({}, 'c', nil, function() hs.eventtap.keyStroke({}, 'up') k.triggered = true end)
+k:bind({}, 't', nil, function() hs.eventtap.keyStroke({}, 'down') k.triggered = true end)
+
+k:bind({}, 'l', nil, function() hs.eventtap.keyStroke({}, 'pageup') k.triggered = true end)
+k:bind({}, 's', nil, function() hs.eventtap.keyStroke({}, 'pagedown') k.triggered = true end)
+
+k:bind({}, 'g', nil, function() hs.eventtap.keyStroke({}, 'home') k.triggered = true end)
+k:bind({}, 'r', nil, function() hs.eventtap.keyStroke({}, 'end') k.triggered = true end)
+
+k:bind({}, 'd', nil, function() hs.eventtap.keyStroke({}, 'delete') k.triggered = true end)
+k:bind({}, 'b', nil, function() hs.eventtap.keyStroke({}, 'forwarddelete') k.triggered = true end)
+
+k:bind({}, 'm', nil, function() hs.eventtap.keyStroke({}, 'return') k.triggered = true end)
+
+-- Enter Hyper Mode
+hyperPressed = function()
   k.triggered = false
   k:enter()
 end
 
--- Leave Hyper Mode when F19 (left control) is pressed,
---   send ESCAPE if no other keys are pressed.
-releasedF19 = function()
+-- Leave Hyper Mode when F19 (caps) is pressed,
+-- send ESCAPE if no other keys are pressed.
+f19Released = function()
   k:exit()
   if not k.triggered then
-    hs.eventtap.keyStroke({}, 'ESCAPE')
+    hs.eventtap.keyStroke({}, 'escape')
   end
 end
 
--- Bind the Hyper key
-f19 = hs.hotkey.bind({}, 'F19', pressedF19, releasedF19)
+-- Leave Hyper Mode when F20 (delete) is pressed,
+-- send RETURN if no other keys are pressed.
+f20Released = function()
+  k:exit()
+  if not k.triggered then
+    hs.eventtap.keyStroke({}, 'delete')
+  end
+end
+
+-- Bind the Hyper key to Caps
+hs.hotkey.bind({}, 'F19', hyperPressed, f19Released)
+
+-- Bind the Hyper key to Return
+hs.hotkey.bind({}, 'F20', hyperPressed, f20Released)
 
 hs.hotkey.bind({"ctrl"}, ".", nil, function() hs.eventtap.keyStroke({"cmd"}, ".") end, nil, nil)
 hs.hotkey.bind({"ctrl"}, ",", nil, function() hs.eventtap.keyStroke({"cmd"}, ",") end, nil, nil)
 hs.hotkey.bind({"ctrl"}, ";", nil, function() hs.eventtap.keyStroke({"cmd"}, ";") end, nil, nil)
 hs.hotkey.bind({"cmd", "ctrl"}, ".", nil, function() hs.eventtap.keyStroke({"cmd", "alt"}, ".") end, nil, nil)
--- hs.hotkey.bind({"ctrl"}, "return", nil, function() hs.eventtap.keyStroke({"cmd"}, "return") end, nil, nil)
 
 -- require 'caffeine'
 -- require 'clipboard'
