e3686a0bc99e7c0ae338a93719f243ac69b61c0f
Vladimir Bauer <vbauerster@gmail.com>
2017-06-12 15:46:07 +0300

    Dvorak classic numpad

--- hammerspoon/numpad.lua
+++ hammerspoon/numpad.lua
@@ -1,34 +1,41 @@
-local modalKey = hs.hotkey.modal.new(hyper, 'U', 'Numpad mode')
-modalKey:bind('', 'escape', function() modalKey:exit() end)
+-- local log = hs.logger.new("numpad", "info")
+
+local modalKey = hs.hotkey.modal.new(hyper, 'return', 'Npad mode')
+modalKey:bind({}, 'escape', function() modalKey:exit() end)
+
+local exit_timer = hs.timer.delayed.new(2, function()
+	-- log.i("timer fired")
+	modalKey:exit()
+end)
+
 function modalKey:exited()
-    hs.alert.show('Numpad mode exited', 1)
+	exit_timer:stop()
+	hs.alert.show('Npad mode exited', 1)
 end
 
-local ndic = {
-	u = "pad0",
-	g = "pad4",
-	c = "pad5",
-	r = "pad6",
-	h = "pad1",
-	t = "pad2",
-	n = "pad3"
-}
-ndic[26] = "pad7"
-ndic[28] = "pad8"
-ndic[25] = "pad9"
--- ndic[18] = "pad1"
--- ndic[19] = "pad2"
--- ndic[20] = "pad3"
+local npad = {}
+npad[26] = 'pad0'
+npad[28] = 'pad2'
+npad[25] = 'pad4'
+npad[29] = 'pad6'
+npad[27] = 'pad8'
 
-for k, v in pairs(ndic) do
-	modalKey:bind('', k, function()
+npad[22] = 'pad9'
+npad[23] = 'pad1'
+npad[21] = 'pad3'
+npad[20] = 'pad5'
+npad[19] = 'pad7'
+
+for k, v in pairs(npad) do
+	modalKey:bind({}, k, function()
 		-- Pressed:
-		hs.eventtap.event.newKeyEvent('', v, true):post()
+		exit_timer:start()
+		hs.eventtap.event.newKeyEvent({}, v, true):post()
 	end, function()
 		-- Released:
-		hs.eventtap.event.newKeyEvent('', v, false):post()
+		hs.eventtap.event.newKeyEvent({}, v, false):post()
 	end, function()
 		-- Repeat:
-		hs.eventtap.event.newKeyEvent('', v, true):setProperty(hs.eventtap.event.properties.keyboardEventAutorepeat, 1):post()
+		hs.eventtap.event.newKeyEvent({}, v, true):setProperty(hs.eventtap.event.properties.keyboardEventAutorepeat, 1):post()
 	end)
 end
