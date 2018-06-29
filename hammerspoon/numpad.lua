e2f09f6685126c9b111e5df7380863ecb5308435
Vladimir Bauer <vbauerster@gmail.com>
2018-06-29 10:09:18 +0300

    better numpad

--- hammerspoon/numpad.lua
+++ hammerspoon/numpad.lua
@@ -2,8 +2,10 @@
 
 local modalKey = hs.hotkey.modal.new({'command', 'ctrl', 'alt', 'shift'}, 'm', 'Npad mode')
 modalKey:bind({}, 'q', function() modalKey:exit() end)
+modalKey:bind({}, 'j', function() modalKey:exit() hs.eventtap.keyStroke({}, 'j') end)
+modalKey:bind({}, 'k', function() modalKey:exit() hs.eventtap.keyStroke({}, 'k') end)
 
-local exit_timer = hs.timer.delayed.new(4, function()
+local exit_timer = hs.timer.delayed.new(5, function()
 	-- log.i("timer fired")
 	modalKey:exit()
 end)
@@ -17,22 +19,22 @@ function modalKey:exited()
 	hs.alert.show('Exit Npad mode', 1)
 end
 
+-- 7531902468
+-- hs.inspect(hs.keycodes.map)
 local npad = {}
-npad[50] = 'pad*'
-npad[18] = 'pad+'
-npad[24] = 'pad='
+-- npad[48] = 'delete'
 
-npad[26] = 'pad0'
-npad[28] = 'pad2'
-npad[25] = 'pad4'
-npad[29] = 'pad6'
-npad[27] = 'pad8'
+npad[4] = 'pad0'
+npad[38] = 'pad2'
+npad[40] = 'pad4'
+npad[37] = 'pad6'
+npad[41] = 'pad8'
 
-npad[22] = 'pad9'
-npad[23] = 'pad1'
-npad[21] = 'pad3'
-npad[20] = 'pad5'
-npad[19] = 'pad7'
+npad[3] = 'pad1'
+npad[2] = 'pad3'
+npad[1] = 'pad5'
+npad[0] = 'pad7'
+npad[5] = 'pad9'
 
 for k, v in pairs(npad) do
 	modalKey:bind({}, k, function()
