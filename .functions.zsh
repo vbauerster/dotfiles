a233bbe7c1b6bf27580a483399060b0c96df1999
Vladimir Bauer <vbauerster@gmail.com>
2019-03-05 14:20:52 +0300

    drop unused

--- .functions.zsh
+++ .functions.zsh
@@ -203,37 +203,6 @@ tmss() {
   tmux switch-client -t "$session"
 }
 
-# ch - browse chrome history
-ch() {
-  local cols sep
-  export cols=$(( COLUMNS / 3 ))
-  export sep='{::}'
-
-  cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
-  sqlite3 -separator $sep /tmp/h \
-    "select title, url from urls order by last_visit_time desc" |
-  ruby -ne '
-    cols = ENV["cols"].to_i
-    title, url = $_.split(ENV["sep"])
-    len = 0
-    puts "\x1b[36m" + title.each_char.take_while { |e|
-      if len < cols
-        len += e =~ /\p{Han}|\p{Katakana}|\p{Hiragana}|\p{Hangul}/ ? 2 : 1
-      end
-    }.join + " " * (2 + cols - len) + "\x1b[m" + url' |
-  fzf --ansi --multi --no-hscroll --tiebreak=index |
-  sed 's#.*\(https*://\)#\1#' | xargs open
-}
-
-# so - my stackoverflow favorites
-so() {
-    stackoverflow-favorites |
-    fzf --ansi --reverse --with-nth ..-2 --tac --tiebreak index |
-    awk '{print $NF}' | while read -r line; do
-      open "$line"
-    done
-}
-
 # some custotm functions
 # ----------------------
 
